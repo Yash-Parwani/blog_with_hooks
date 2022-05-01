18980ee4f4a4f3cf1b1861a8c130150d4f618021
Yash Parwani <parwaniyash5610@gmail.com>
2022-05-01 19:03:36 +0530

    starting react blogs with hooks

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+const firebaseConfig = {
+    apiKey: "AIzaSyD0vdSGIs2fIsJl10fPE6zb3-Gj8bjWpA4",
+    authDomain: "blog-with-hooks-7c071.firebaseapp.com",
+    projectId: "blog-with-hooks-7c071",
+    storageBucket: "blog-with-hooks-7c071.appspot.com",
+    messagingSenderId: "497551958079",
+    appId: "1:497551958079:web:392b9b2865c8835b12988a"
+  };
+
+  // initialize firebase
+
+ firebase.initializeApp(firebaseConfig);
