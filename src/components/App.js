3ace518119724547bba8df88786ecaf84f700e06
Yash Parwani <parwaniyash5610@gmail.com>
2022-05-01 23:21:21 +0530

    created createPost component to handle form

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
-
+import {BrowserRouter as Router ,Routes,Route} from "react-router-dom"
+import {Navbar, Home, CreatePost ,PostDetail} from './index.js'
 function App() {
   return (
-    <div className="App">
-          Starting with blog
+    <Router>
+      
+      <div className="Container">
+          <Navbar/>
+          <h1>Starting with react web hook</h1>
+          <Routes>
+           <Route exact path="/" element={<Home/>} />
+           <Route exact path="/post/:postId" element={<PostDetail/>} />
+           <Route exact path="/create-post"  element={<CreatePost />}/>
+           
+         </Routes>
     </div>
+
+    </Router>
+    
   );
 }
 
---
+++ src/components/CreatePost.js
@@ -0,0 +1,59 @@
+import {useState} from 'react'
+
+
+
+function CreatePost() {
+    // using hooks so that we can manipulate what user is typing easily
+    const [title,setTitle] = useState();
+    const [content,setContent] = useState();
+    const [subTitle,setSubTitle] = useState();
+
+
+
+   function handleSubmit  (event){
+        // first we will prevent default submit so that page does not perform a reload
+        event.preventDefault();
+        
+        
+        
+        
+        // checking if on submitiing we get everything as we wanted
+        console.log("title",title);
+        console.log("Subtitle",subTitle);
+        console.log("content",content);
+        
+        }
+        
+    return (
+      <div className="CreatePost">
+           <h1>Create Post</h1>
+           <form onSubmit={handleSubmit}>
+               <div className="form-field">
+                   <label>Title</label>
+                   <input value={title} onChange={(event) =>{
+                       setTitle(event.target.value);
+                    //    event.target.value is anything that user types in in the text area
+                   }}/>
+               </div>
+               <div className="form-field">
+                   <label>Sub Title</label>
+                   <input value={subTitle} onChange={(event) =>{
+                       setSubTitle(event.target.value);
+                    //    event.target.value is anything that user types in in the text area
+                   }}/>
+               </div>
+               <div className="form-field">
+                   <label>Content</label>
+                   {/* Using text area since content will be over  a large area */}
+                   <textarea value={content} onChange={(event) =>{
+                       setContent(event.target.value);
+                    //    event.target.value is anything that user types in in the text area
+                   }}></textarea>
+               </div>
+               <button className="create-post-btn">Create Post</button>
+           </form>
+      </div>
+    );
+  }
+  
+  export default CreatePost;
